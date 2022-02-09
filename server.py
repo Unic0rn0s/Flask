5c67aac223c167c4bb1279913c7b68c7138ab5d2
Unic0rn0s <78038622+Unic0rn0s@users.noreply.github.com>
2022-02-09 22:43:23 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,16 +1,34 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask('Sus')
 
 
 @app.route('/')
-def index():
+def start():
     return '<h1>Миссия Колонизация Марса</h1>'
 
 
 @app.route('/index')
-def func1():
+def index():
     return '<h1>И на Марсе будут яблони цвести!</h1>'
+
+
+@app.route('/promotion')
+def promotion():
+    txt = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
+           'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!',
+           'Присоединяйся!']
+    return '</br>'.join(txt)
+
+
+@app.route('/user/<int:id>')
+def func2(id):
+    return str(id)
+
+
+@app.route('/rianna')
+def rianna():
+    return f'<img src="{url_for("static", filename="rianna.jpeg")}">'
 
 
 app.run(host='localhost', port=8080, debug=True)
